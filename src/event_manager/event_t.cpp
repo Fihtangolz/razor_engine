c0c601b6f1ebcd6044b88f4bbe64fdc4304a83c7
Opokin D.S <mamhigt@mail.ru>
2019-07-06 13:01:33 +0700

    small event type fix

--- src/event_manager/event_t.cpp
+++ src/event_manager/event_t.cpp
@@ -1,5 +1,12 @@
-#include "event_t.cpp"
+#include "event_t.hpp"
 
-event_type event_t::get_type_id() override {
+template<typename Inheritor>
+std::time_t event_t<Inheritor>::get_timestamp() {
+    return std::time_t{};
+}
+
+template<typename Inheritor>
+event_type event_t<Inheritor>::get_type_id() {
     return Inheritor::TYPE_ID;
 }
+
--- src/event_manager/event_t.hpp
+++ src/event_manager/event_t.hpp
@@ -2,23 +2,26 @@
 #define RAZOR_ENGINE_COMMAND_T_HPP
 
 #include <ctime>
+#include <cstdint>
 
 enum class event_type: int64_t;
 
 //Accepts components and change it after execute call
+//Should not have side effects
 //NOTE: also it called command pattern
 class event_i {
 public:
-    virtual std::time_t get_timestamp() = 0; //TODO
+    virtual std::time_t get_timestamp() = 0;
     virtual event_type get_type_id() = 0;
     virtual void execute() = 0;
     virtual ~event_i() = default;
 };
 
 template<typename Inheritor>
-class event_t : event_i {
+class event_t : public event_i {
 public:
     event_type get_type_id() override;
+    std::time_t get_timestamp() override;
 };
 
 
