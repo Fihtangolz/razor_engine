bf20853b3d42dc078ab82cff1eda296c3135d722
Opokin D.S <mamhigt@mail.ru>
2019-07-06 13:03:51 +0700

    add todo for event_type enum

--- src/event_manager/inbuilt_event.hpp
+++ src/event_manager/inbuilt_event.hpp
@@ -5,24 +5,33 @@
 
 #include "event_t.hpp"
 
+/* TODO
+ * struct event_type {
+    enum : int64_t {
+        ENGINE_TERMINATE,
+        ENGINE_RESUME,
+        ENGINE_SUSPEND,
+    };
+
+    std::string stringify() {
+        switch(et) {
+            case event_type::ENGINE_TERMINATE:
+                return "ENGINE_TERMINATE";
+            case event_type::ENGINE_RESUME:
+                return "ENGINE_RESUME";
+            case event_type::ENGINE_SUSPEND:
+                return "ENGINE_SUSPEND";
+        }
+    }
+}; */
+
 enum class event_type: int64_t {
     ENGINE_TERMINATE,
     ENGINE_RESUME,
     ENGINE_SUSPEND,
 };
 
-std::string stringify(event_type et) {
-    switch(et) {
-        case event_type::ENGINE_TERMINATE:
-            return "ENGINE_TERMINATE";
-        case event_type::ENGINE_RESUME:
-            return "ENGINE_RESUME";
-        case event_type::ENGINE_SUSPEND:
-            return "ENGINE_SUSPEND";
-    }
-}
-
-class engine_terminate_t : event_t<engine_terminate_t> {
+class engine_terminate_t : public event_t<engine_terminate_t> {
 public:
     static const event_type TYPE_ID = {event_type::ENGINE_TERMINATE};
     void execute() override {}
