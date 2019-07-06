86fc4bea053ca77bf5268bf77067ac0b4649ae38
Opokin D.S <mamhigt@mail.ru>
2019-07-06 13:04:40 +0700

    add hit for add single event listener

--- src/event_manager/event_manager_t.hpp
+++ src/event_manager/event_manager_t.hpp
@@ -1,19 +1,33 @@
 #ifndef RAZOR_ENGINE_EVENT_MANAGER_T_HPP
 #define RAZOR_ENGINE_EVENT_MANAGER_T_HPP
 
-#include <queue>
+#include <vector>
 #include <functional>
 
 #include "event_t.hpp"
+#include "inbuilt_event.hpp" //TODO
 
 using event_listener_t = std::function<void()>*;
 
 class event_manager_t {
 private:
-    std::queue<event_i> event_queue;
+    std::vector<event_i*> event_queue;
+    event_listener_t _listener; //TODO
 public:
+    void process_event() {
+        for(auto&& event : event_queue) {
+            event->execute();
+            if(event->get_type_id() == engine_terminate_t::TYPE_ID) {
+                _listener->operator()();
+            }
+        }
+    }
+    template<typename EventType, typename... Args>
+    void send_event(Args&&... args) {
+        event_queue.push_back(new EventType{args...});
+    }
     void subscribe(event_type et, event_listener_t listener) {
-
+        _listener = listener;
     }
     void unsubcribe(event_type et, event_listener_t listener) {
 
