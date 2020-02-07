d4ce32fe108e47cc30e828ce8425a132e9860fa6
Dmitry <mamhigtt@gmail.com>
2020-02-08 01:55:47 +0700

    update game engine signature

--- src/game_engine.rs
+++ src/game_engine.rs
@@ -1,27 +1,40 @@
 use crate::ecs;
 use ecs::system::System;
 use crate::render_sys::RenderSys;
+use crate::input_sys::InputSys;
 
-pub struct GameEngine<S: System> {
-    systems: [S]
+use winit::{
+    window,
+    event::Event,
+};
+
+use std::sync::mpsc::Receiver;
+
+pub struct GameEngine {
+    systems: [Box<dyn System>; 2], //TODO: it is compile time know array, box not needed this
 }
 
-impl<S> GameEngine<S> {
-    pub fn new() -> Self {
-        Self { systems: [ RenderSys{} ] }
+impl GameEngine {
+    pub fn new(resiver: Receiver<Event<'static, ()>>) -> Self {
+        Self { systems: [
+            Box::new(RenderSys{}), 
+            Box::new(InputSys::new(resiver))
+        ] }
     }
     
     pub fn configure(self) -> Self {
         self
     }
 
-    pub fn init(self) -> Self {
+    pub fn init(mut self) -> Self {
         self
     }
 
-    pub fn idle(self) {
+    pub fn idle(mut self) {
         loop {
-            self.systems.map(|s| s.process());
+            for system in &mut self.systems {
+                system.process();
+            }
         }
     }
 }
